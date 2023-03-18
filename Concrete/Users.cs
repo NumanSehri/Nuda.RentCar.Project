1baa1694a5aee92b4e71a1bdc796c382044055fa
NumanSehri <numansehri@gmail.com>
2023-03-19 02:01:08 +0300

    Müşteri ve Kullanıı

---
+++ Concrete/Users.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Users:IEntitiy
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public PasswordPropertyTextAttribute  Password{ get; set; }
+    }
+}
